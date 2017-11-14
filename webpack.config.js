612fa1a379bf894595f766052dcc588d815f1659
Chris <chris@chrisababan.com>
2017-11-14 14:55:33 -0500

    Setup development environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const dist = path.join(__dirname, "dist");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js'
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: dist,
+    compress: true,
+    port: 3000
+  },
   output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'dist')
-  }
+    filename: '[name].bundle.js',
+    path: dist
+  },
+  plugins: [
+    new CleanWebpackPlugin([ 'dist' ]),
+    new HtmlWebpackPlugin({ title: 'My Barebone Front End App' })
+  ]
 };
