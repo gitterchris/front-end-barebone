843cbce3798870f17681fdca6bdc9f7b97d1e0eb
Chris <chris@chrisababan.com>
2018-01-05 16:02:41 -0500

    Fix build issues

--- webpack.common.js
+++ webpack.common.js
@@ -11,11 +11,7 @@ module.exports = {
         test: /\.js$/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/transform-runtime']
-          }
+          loader: 'babel-loader'
         }
       }
     ]
